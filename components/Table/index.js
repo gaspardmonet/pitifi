711da801f7721a05473f538f82e6474fdbfa3df6
gaspardmonet <marianoparra1118@gmail.com>
2022-11-22 12:34:02 +0200

    Update index.js

--- components/Table/index.js
+++ components/Table/index.js
@@ -1,7 +1,91 @@
-//tables component's (appearing in manage property and security page) code will appear here
+import { PaperClipIcon } from '@heroicons/react/20/solid'
 
-// https://tailwindui.com/components/application-ui/lists/tables#component-c54854f065bce97d355355a82bfd13cf
-
-export default function Loader() {
-  return <div>Table</div>;
+export default function Example() {
+  return (
+    <>
+      <div>
+        <h3 className="text-lg font-medium leading-6 text-gray-900">Applicant Information</h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+      </div>
+      <div className="mt-5 border-t border-gray-200">
+        <dl className="divide-y divide-gray-200">
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+            <dt className="text-sm font-medium text-gray-500">Full name</dt>
+            <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              <span className="flex-grow">Margot Foster</span>
+              <span className="ml-4 flex-shrink-0">
+                <button
+                  type="button"
+                  className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Update
+                </button>
+              </span>
+            </dd>
+          </div>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+            <dt className="text-sm font-medium text-gray-500">Application for</dt>
+            <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              <span className="flex-grow">Backend Developer</span>
+              <span className="ml-4 flex-shrink-0">
+                <button
+                  type="button"
+                  className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Update
+                </button>
+              </span>
+            </dd>
+          </div>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+            <dt className="text-sm font-medium text-gray-500">Email address</dt>
+            <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              <span className="flex-grow">margotfoster@example.com</span>
+              <span className="ml-4 flex-shrink-0">
+                <button
+                  type="button"
+                  className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Update
+                </button>
+              </span>
+            </dd>
+          </div>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+            <dt className="text-sm font-medium text-gray-500">Salary expectation</dt>
+            <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              <span className="flex-grow">$120,000</span>
+              <span className="ml-4 flex-shrink-0">
+                <button
+                  type="button"
+                  className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Update
+                </button>
+              </span>
+            </dd>
+          </div>
+          <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+            <dt className="text-sm font-medium text-gray-500">About</dt>
+            <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              <span className="flex-grow">
+                Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
+                qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure
+                nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+              </span>
+              <span className="ml-4 flex-shrink-0">
+                <button
+                  type="button"
+                  className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Update
+                </button>
+              </span>
+            </dd>
+          </div>
+     
+        </dl>
+      </div>
+    </>
+  )
 }
